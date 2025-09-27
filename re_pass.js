e1ed3d50226080710012fd6e199e917bae32a471
ELFWEGOS <mizanmazin49@gmail.com>
2025-09-27 13:56:50 +0100

    Add files via upload

---
+++ re_pass.js
@@ -0,0 +1,39 @@
+let mdp = ""
+const length = document.getElementById("inp_len")
+const msg = document.getElementById("result")
+
+
+function gen_chr () {
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*";
+
+const randomIndex = Math.floor(Math.random() * chars.length);
+const randomChar = chars[randomIndex];
+
+return randomChar
+
+}
+function generate() {
+    const length_val = parseInt(length.value)
+
+    mdp = ""
+    let i = 0
+
+
+    while (i < length_val){ 
+        let chr = gen_chr()
+        mdp += chr
+        i += 1
+        
+    } 
+   msg.textContent = 'Votre Mdp est : "'+mdp+'"'
+
+}
+function copier() {
+    if (mdp === "") {
+        alert("Génère d'abord un mot de passe poto 😅")
+        return
+    }
+    navigator.clipboard.writeText(mdp).then(() => {
+        alert("Mot de passe copié ! ✅")
+    })
+}
